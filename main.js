20c6624612d0da4bbf2f7f70f5a44da5014e3149
06mrx <0611mrx@gmail.com>
2024-07-12 22:39:46 +0800

    feat: add image once view stealer

--- main.js
+++ main.js
@@ -15,7 +15,12 @@ const client = new Client({
 client.on('ready', () => {
     console.log('Client is ready!');
 });
-client.on('message_create', async (message) => {
+
+// client.on('message', msg => {
+    
+// });
+
+client.on('message', async (message) => {
     if (message.location) {
         // console.log(message.location);
         let user = await message.getContact();
@@ -54,15 +59,86 @@ client.on('message_create', async (message) => {
                 console.error('Error:', error);
             });
 
-    } else {
-        // const chat = await message.getChat();
-        // let user = await message.getContact();
-        // // await chat.sendMessage(`Hello @${user.id.user}`, {
-        // //     mentions: [user]
-        // // });
+    } else if(message.hasMedia) {
+        //jika gambar sekali lihat, body nya kosong
+        if(message.body == '') {
+            console.log('once view')
+            const media = await message.downloadMedia();
+            // client.sendMessage(message.from, media);
+            console.log('ada media')
+            const chat = await message.getChat();
+            let user = await message.getContact();
+            await chat.sendMessage(media, {
+                caption: `Ini gambarnya @${user.id.user}`
+            });
+        }
+        
         // console.log(user.name)
 
 
+    } else {
+        console.log('ada pesan')
+        if (message.body == 'ping') {
+            message.reply('pong');
+        }
+    }
+});
+
+
+client.on('message_createdd', async (message) => {
+    if (message.location) {
+        // console.log(message.location);
+        let user = await message.getContact();
+        const chat = await message.getChat();
+
+        var responseMsg = "dari " + user.name + "Nomor : " + user.id.user + " latitude : " + message.location.latitude + " longitude : " + message.location.longitude
+        
+        console.log(responseMsg);
+        const url = process.env.sheet_url;
+
+        const data = {
+            Nama: user.name,
+            Nomor: user.id.user,
+            Latitude: message.location.latitude,
+            Longitude: message.location.longitude,
+            Url: `https://maps.google.com/?q=${message.location.latitude},${message.location.longitude}`
+        };
+
+        const responseMsg2 = `✨ Terima Kasih! ✨\n\nData Anda telah berhasil dikirim. Berikut adalah informasi yang kami terima:\n\n📌 Nama: ${data.Nama}\n📞 Nomor: ${data.Nomor}\n🌍 Lokasi:\n    Latitude: ${data.Latitude}\n    Longitude: ${data.Longitude}\n📍 [Lihat di Google Maps](${data.Url})\n\nJika ada informasi lebih lanjut yang Anda butuhkan, jangan ragu untuk menghubungi kami.\n\nTerima kasih atas kerjasamanya! 😊`;
+
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams(data).toString()
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                
+                 chat.sendMessage(responseMsg2);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+    } else if(message.hasMedia) {
+        const media = await message.downloadMedia();
+        // client.sendMessage(message.from, media);
+        console.log('ada media')
+        const chat = await message.getChat();
+        let user = await message.getContact();
+        await chat.sendMessage(media);
+        // console.log(user.name)
+
+
+    } else {
+        console.log('ada pesan')
+        if (message.body == 'ping') {
+            message.reply('pong');
+        }
     }
 
 
